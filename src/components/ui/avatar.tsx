63deeced2c1c6fc0569ab0122cc571f9c6c1188a
САДУЛЛАЕВ ЭЛЬБРУС МАХАЧЕВИЧ 1ТЭ425 <st.sadullaevem@kmt.moscow>
2026-03-21 22:41:28 +0300

    Fix avatar shape to always stay circular

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -2,7 +2,7 @@ import * as AvatarPrimitive from "@radix-ui/react-avatar";
 import { cn, getInitials } from "@/lib/utils";
 
 export function Avatar({ className, ...props }: React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>) {
-  return <AvatarPrimitive.Root className={cn("relative flex h-11 w-11 shrink-0 overflow-hidden rounded-full", className)} {...props} />;
+  return <AvatarPrimitive.Root className={cn("relative aspect-square flex h-11 w-11 shrink-0 overflow-hidden rounded-full", className)} {...props} />;
 }
 
 export function AvatarImage(props: React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>) {
